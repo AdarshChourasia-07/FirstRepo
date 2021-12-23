8b139a12c145bd49210c19f2cd2cb1ed65106924
Adarsh Chourasia <adarshchourasia7@gmail.com>
2021-12-23 17:27:37 +0530

    initial commit

---
+++ Sum.java
@@ -0,0 +1,11 @@
+public class Sum
+{
+    public static void main(String args [])
+    {
+        System.out.println("Hello");
+        int a=5;
+        int b=2;
+        System.out.println("Sum is ="+(a+b));
+        
+    }
+}
